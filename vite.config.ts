c1ab24d2140a0ab5c416129fd4316624e9b3fcb0
amkkr <55781271+amkkr@users.noreply.github.com>
2025-07-01 12:34:15 +0900

    add: vitest追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Pages({
+      dirs: 'src/pages',
+      extensions: ['tsx', 'ts']
+    })
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  }
 })
